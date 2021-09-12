2d6c57b9f40b6a8ba266231ce7fbf76efe69fc22
Soumyo78 <soumyo78@yahoo.com>
2021-09-12 18:26:04 +0530

    on scroll navbar actice class change applied

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,46 @@ import Portfolio from "./components/Portfolio";
 import Work from "./components/Work";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
+import ReactDOM from "react-dom";
+import { useEffect } from "react";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    let sectionMainContainer = document.querySelector(
+      ".sections-main-container"
+    );
+
+    sectionMainContainer.onscroll = function () {
+      let sectionHeight = sectionMainContainer.scrollTop;
+      let currentViewHeight = window.innerHeight - 80;
+
+      if (sectionHeight >= 0 && sectionHeight < currentViewHeight) {
+        ReactDOM.findDOMNode(document.querySelector(".active")).classList.remove("active");
+        ReactDOM.findDOMNode(document.querySelector("#nav-home")).classList.add("active");
+      }
+
+     if (sectionHeight >= currentViewHeight && sectionHeight <= currentViewHeight*2) {
+        ReactDOM.findDOMNode(document.querySelector(".active")).classList.remove("active");
+        ReactDOM.findDOMNode(document.querySelector("#nav-portfolio")).classList.add("active");
+      }
+
+      if (sectionHeight >= currentViewHeight*2 && sectionHeight <= currentViewHeight*3) {
+        ReactDOM.findDOMNode(document.querySelector(".active")).classList.remove("active");
+        ReactDOM.findDOMNode(document.querySelector("#nav-work")).classList.add("active");
+      }
+
+      if (sectionHeight >= currentViewHeight*3 && sectionHeight <= currentViewHeight*4) {
+        ReactDOM.findDOMNode(document.querySelector(".active")).classList.remove("active");
+        ReactDOM.findDOMNode(document.querySelector("#nav-testimonials")).classList.add("active");
+      }
+
+      if (sectionHeight >= currentViewHeight*4 && sectionHeight <= currentViewHeight*5) {
+        ReactDOM.findDOMNode(document.querySelector(".active")).classList.remove("active");
+        ReactDOM.findDOMNode(document.querySelector("#nav-contact")).classList.add("active");
+      }
+    };
+  }, []);
+
   return (
     <main className="app-main-container">
       <Navbar />
@@ -20,6 +58,6 @@ function App() {
       </div>
     </main>
   );
-}
+};
 
 export default App;
